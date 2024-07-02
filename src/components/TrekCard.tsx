f0b5cf2e5816f9070315639fce3c65214345a7da
ChenHaoJie9527 <a18520145493@163.com>
2024-07-02 16:06:15 +0800

    定位上移

--- src/components/TrekCard.tsx
+++ src/components/TrekCard.tsx
@@ -4,7 +4,7 @@ import Packed from "./base/Packed";
 
 export default function TrekCard() {
   return (
-    <Card className=" shadow-xl border-none min-w-[740px] ">
+    <Card className=" shadow-xl border-none min-w-[740px] relative -top-12">
       <CardHeader className="bg-[#c9f299] rounded-t-lg p-4">
         <CardTitle className="flex items-center justify-between">
           <Status className="bg-[#e2fcef]" />
