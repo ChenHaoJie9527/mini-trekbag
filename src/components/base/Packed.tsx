09034ccdb3346c1ae2924cbe1f4b61e830b30f65
ChenHaoJie9527 <a18520145493@163.com>
2024-07-02 16:10:33 +0800

    字体间距

--- src/components/base/Packed.tsx
+++ src/components/base/Packed.tsx
@@ -4,7 +4,7 @@ interface Props {
 }
 export default function Packed({ current = 0, total = 3 }: Props) {
   return (
-    <div className="text-lg font-normal">
+    <div className="text-lg font-normal tracking-[1px] opacity-70">
       <span className=" font-[500]">{current}</span> / {total} Items Packed
     </div>
   );
